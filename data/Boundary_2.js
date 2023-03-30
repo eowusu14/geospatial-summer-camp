5805585c5e42e5b89f2db2836f63b0b45d5f0406
Hydra33 <64472767+Hydra33@users.noreply.github.com>
2023-03-30 21:44:24 +0000

    initial commit

---
+++ data/Boundary_2.js
@@ -0,0 +1,9 @@
+var json_Boundary_2 = {
+"type": "FeatureCollection",
+"name": "Boundary_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1", "SHAPE_Leng": 0.0, "SHAPE_Area": 0.0 }, "geometry": { "type": "MultiPolygon", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2", "SHAPE_Leng": 9001.97659, "SHAPE_Area": 3111119.4642400001 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -1.547079564858434, 6.679887374196833 ], [ -1.549989011362962, 6.679669556633273 ], [ -1.55028086639563, 6.684629921932152 ], [ -1.553519097556851, 6.684850855918794 ], [ -1.55342712040979, 6.685773398505885 ], [ -1.552050173249038, 6.686727276542369 ], [ -1.54849205051083, 6.687241241481132 ], [ -1.547079564858434, 6.679887374196833 ] ] ] ] } }
+]
+}
